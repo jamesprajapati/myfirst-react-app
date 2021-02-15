4d75e56fa61776c8824f88b238aae538e2189b7a
jamesprajapati <g.m.prajapati9827@gmail.com>
2021-02-15 23:06:21 +0530

    deploy my first react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Avatar from './img/wenzix.png'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Hello I am Gaurav</h1>
+     <img src={Avatar} alt="Avtar" />
+    </div>
+  );
+}
+
+export default App;
