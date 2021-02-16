cb050eee579b5d2b4879d71f1d013d4f7d31ccaf
jamesprajapati <g.m.prajapati9827@gmail.com>
2021-02-17 00:38:14 +0530

    sidebar working

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import Avatar from './img/wenzix.png'
 import './App.css';
+import Navbar from "./Components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-           <h2>
-                          
-           </h2>
+    <>
+      
+      <Router>
+        <Navbar/>
+         <Switch>
+            <Route path='/' />
+         </Switch>
+      </Router>    
+      <center>
           <img src={Avatar} alt="Avtar" className="center"/>
           <h1>Hello I am Gaurav Prajapati</h1>
-    </div>
+      </center>
+    
+    </>
+  
+    
   );
 }
 
